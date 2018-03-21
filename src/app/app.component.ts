4f53f016daa3d37811aaf7d886984d8b7e50232a
MarkoFirst <babiichuck.vadim@yandex.ru>
2018-03-21 16:50:54 +0200

    TS-12 добавил константы

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import 'rxjs/add/operator/map';
 import {StoreService} from './services/store/store.service';
 
+const LOGGED = 'logged';
+const USER_IN_MY_APP = 'userInMyApp';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,8 +16,8 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    if (JSON.parse(localStorage.getItem('logged'))) {
-      this.storeService.setUser(JSON.parse(localStorage.getItem('userInMyApp')));
+    if (JSON.parse(localStorage.getItem(LOGGED))) {
+      this.storeService.setUser(JSON.parse(localStorage.getItem(USER_IN_MY_APP)));
     }
   }
 
